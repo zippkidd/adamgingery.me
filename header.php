be9fb48735b71620ad8ac8360fefa42cca14eeba
zippkidd <thomastylek@gmail.com>
2020-06-14 19:25:20 -0400

    Investigating astra header menu calls

--- header.php
+++ header.php
@@ -38,13 +38,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 >
 	<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?></a>
 
+    <span class="test before astra_header_before"></span>
 	<?php astra_header_before(); ?>
-
-	<?php astra_header(); ?>
-
+    <span class="test after astra_header_before before astra_header"></span>
+	<?php astra_header(); //this is the header#masthead part ?>
+    <span class="test after astra_header before astra_header_after"></span>
 	<?php astra_header_after(); ?>
-
+    <span class="test after astra_header_after before astra_content_before"></span>
 	<?php astra_content_before(); ?>
+    <span class="test after astra_content_before"></span>
 
 	<div id="content" class="site-content">
 
