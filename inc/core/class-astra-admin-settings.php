5f5a4513cb6087d644724f57b70fbd42ec848c8b
zippkidd <thomastylek@gmail.com>
2020-06-28 16:39:42 -0400

    added $whiteLabel

--- inc/core/class-astra-admin-settings.php
+++ inc/core/class-astra-admin-settings.php
@@ -17,6 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 
+	$whiteLabel = "adamgingery.com";
+
 	/**
 	 * Astra Admin Settings
 	 */
@@ -86,8 +88,8 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 		 * Admin settings init
 		 */
 		public static function init_admin_settings() {
-			self::$menu_page_title = apply_filters( 'astra_menu_page_title', __( 'adamgingery.me Options', 'astra' ) );
-			self::$page_title      = apply_filters( 'astra_page_title', __( 'adamgingery.me', 'astra' ) );
+			self::$menu_page_title = apply_filters( 'astra_menu_page_title', __( $whiteLabel . ' Options', 'astra' ) );
+			self::$page_title      = apply_filters( 'astra_page_title', __( $whiteLabel, 'astra' ) );
 			self::$plugin_slug     = self::get_theme_page_slug();
 
 			add_action( 'admin_enqueue_scripts', __CLASS__ . '::register_scripts' );
@@ -571,7 +573,7 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 								<?php if ( $ast_icon ): ?>
 									<img src="<?php echo esc_url( ASTRA_THEME_URI . 'inc/assets/images/astra.svg' ); ?>" class="ast-theme-icon" alt="<?php echo esc_attr( self::$page_title ); ?> " >
 								<?php else: ?>
-									<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.jpg' ?>" class="ast-theme-icon" alt="adamgingery.me Theme Logo" >
+									<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.jpg' ?>" class="ast-theme-icon" alt="<?php echo $whiteLabel; ?> Theme Logo" >
 								<?php endif; ?>
 								<?php do_action( 'astra_welcome_page_header_title' ); ?>
 								</a>
