75deeb59100205dd9aede42880e40467b501934a
zippkidd <thomastylek@gmail.com>
2020-07-11 16:17:31 -0400

    add blog-page.php check for astra_schema_body()

--- functions.php
+++ functions.php
@@ -162,6 +162,36 @@ function astra_primary_navigation_markup() {
 }
 
 add_action( 'astra_masthead_content', 'astra_primary_navigation_markup', 10 );
+
+
+/**
+ * Adds schema tags to the body classes.
+ *
+ * @since 1.0.0
+ */
+function astra_schema_body() {
+
+	if ( true !== apply_filters( 'astra_schema_enabled', true ) ) {
+		return;
+	}
+
+	// Check conditions.
+	$is_blog = ( is_home() || is_archive() || is_attachment() || is_tax() || is_single() || is_page_template('blog-page.php') ) ? true : false;
+
+	// Set up default itemtype.
+	$itemtype = 'WebPage';
+
+	// Get itemtype for the blog.
+	$itemtype = ( $is_blog ) ? 'Blog' : $itemtype;
+
+	// Get itemtype for search results.
+	$itemtype = ( is_search() ) ? 'SearchResultsPage' : $itemtype;
+	// Get the result.
+	$result = apply_filters( 'astra_schema_body_itemtype', $itemtype );
+
+	// Return our HTML.
+	echo apply_filters( 'astra_schema_body', "itemtype='https://schema.org/" . esc_attr( $result ) . "' itemscope='itemscope'" ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+}
 //==
 
 //== Remove Default Astra theme font (astra.woff)
