6c3efa31eaca7ee1a61aa31849ee6dde50bd5f34
zippkidd <thomastylek@gmail.com>
2020-07-11 16:29:49 -0400

    added blog class to header-blog body

--- header-blog.php
+++ header-blog.php
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 <?php astra_head_bottom(); ?>
 </head>
 
-<body <?php astra_schema_body(); ?> <?php body_class(); ?>>
+<body <?php astra_schema_body(); ?> <?php body_class('blog'); ?>>
 
 <?php astra_body_top(); ?>
 <?php wp_body_open(); ?>
