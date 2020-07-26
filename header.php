5de527d07caf3dd551066fea96c0bc1d7e77c69f
zippkidd <thomastylek@gmail.com>
2020-07-26 17:47:59 -0400

    added more styles to font families

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 <?php astra_html_before(); ?>
 <html <?php language_attributes(); ?>>
 <head>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&family=Playfair+Display:ital,wght@0,700;0,800;0,900;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
 <?php astra_head_top(); ?>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
