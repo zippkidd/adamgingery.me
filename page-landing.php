f60c8b6de20abc5bb94f3328aee792d545dab52b
zippkidd <thomastylek@gmail.com>
2020-09-05 12:49:59 -0400

    initialized landing page template

---
+++ page-landing.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Landing Page
+ * 
+ * The template for displaying landing pages.
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+		<?php astra_primary_content_top(); ?>
+
+		<?php astra_content_page_loop(); ?>
+
+		<?php astra_primary_content_bottom(); ?>
+
+	</div><!-- #primary -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
